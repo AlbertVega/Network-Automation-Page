f8862e66bfb55e6dcbdfdc63ecb4d8ed1c6e2d5d
Tavo121 <galvarado13@estudiantec.cr>
2025-11-17 20:57:08 -0600

    Conflictos 2

--- API.py
+++ API.py
@@ -120,3 +120,7 @@ def get_interfaces_status(device: str):
 @app.get("/configure/telemetry")
 def get_telemetry():
     return xe_get_interface_traffic()
+
+@app.get("/configure/telemetry")
+def get_telemetry():
+    return xe_get_interface_traffic()
